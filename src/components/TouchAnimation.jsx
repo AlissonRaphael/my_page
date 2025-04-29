231610c4f12fb38a1d09ae0dcf1fd39214756080
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-04-28 22:51:42 -0300

    feat: create touch component

---
+++ src/components/TouchAnimation.jsx
@@ -0,0 +1,25 @@
+import { Pointer, MoveVertical } from "lucide-react"
+import { motion } from "motion/react"
+
+export default function Touch() {
+  return <motion.div className="flex items-center">
+    <motion.div
+      className="w-6 h-6"
+      animate={{
+        rotate: [-45, -50, -5],
+        translateY: [0, 2, -15],
+      }}
+      transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
+    >
+      <Pointer className="stroke-gray-300" size={22} />
+    </motion.div>
+    <motion.div
+      className="pl-1 text-xs text-gray-300"
+      animate={{
+        translateY: [0, 2, -15],
+      }}
+      transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}>
+        explore
+      </motion.div>
+  </motion.div>
+}
