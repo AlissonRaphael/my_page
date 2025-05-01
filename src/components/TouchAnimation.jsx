43bff4d71e7a43d6de8e5b3c4fbf7cd6629a9394
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-05-01 18:49:35 -0300

    feat: adjusts touch-animation

--- src/components/TouchAnimation.jsx
+++ src/components/TouchAnimation.jsx
@@ -1,33 +1,45 @@
-import { Pointer, MoveVertical } from "lucide-react"
-import { motion } from "motion/react"
+import { Pointer } from "lucide-react"
+import { motion } from "framer-motion"
+import { useState, useEffect } from "react"
 
 export default function Touch() {
-  return <motion.div className="flex items-center">
-    <motion.div
-      className="w-6 h-6"
-      animate={{
-        rotate: [-45, -50, -5],
-        translateY: [0, 2, -15],
-      }}
-      transition={{
-        duration: 1.5,
-        repeat: Infinity,
-        ease: "easeOut",
-      }}
-    >
-      <Pointer className="stroke-gray-300" size={22} />
-    </motion.div>
-    <motion.div
-      className="pl-1 text-xs text-gray-300"
-      animate={{
-        translateY: [0, 2, -15],
-      }}
-      transition={{
-        duration: 1.5,
-        repeat: Infinity,
-        ease: "easeOut",
-      }}>
+  const [scroll, setScroll] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setScroll(true))
+
+    return () => {
+      window.removeEventListener("scroll", () => setScroll(true))
+    }
+  }, [])
+
+  if (scroll) {
+    return null
+  }
+
+  return <div className="absolute inset-y-0 right-0 pr-6 flex">
+    <div className="flex items-center">
+      <motion.div
+        className="w-6 h-6"
+        animate={{ rotate: [-45, -50, -50, -5], translateY: [0, 2, 2, -20] }}
+        transition={transition}
+      >
+        <Pointer className="stroke-gray-300" size={22} />
+      </motion.div>
+      <motion.div
+        className="pl-1 text-xs text-gray-300"
+        animate={{ translateY: [0, 2, 2, -20], }}
+        transition={transition}
+      >
         explore
       </motion.div>
-  </motion.div>
+    </div>
+  </div>
+}
+
+const transition = {
+  duration: 1.2,
+  repeat: Infinity,
+  repeatType: "mirror",
+  ease: "easeOut",
 }
