34ee4e6e9c12a4bc6a009182cd30e69d99db2fd6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-05-05 23:03:57 -0300

    feat: create title-left

---
+++ src/components/TitleLeft.jsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion';
+
+import Typewriter from './Typewriter';
+
+export default function TitleLeft({ title, description, subtitle }) {
+  return <div className="text-xs">
+    <motion.h1
+      className="text-gray-100 uppercase tracking-[0.4em]"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: [0,1,0,1,1,0,1,0,0,1,0,1,0,1,1,0,1] }}
+      transition={{ duration: 1, ease: "anticipate" }}
+      viewport={{ once: true }}
+    >
+      {title}
+    </motion.h1>
+    <motion.div
+      className="text-gray-300 mt-2 max-w-11/12 "
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 0.7, ease: "easeOut" }}
+      viewport={{ once: true }}
+    >
+      {description}
+    </motion.div>
+    <Typewriter
+      className="text-gray-500 mt-2 max-w-11/12 "
+      initialDelay={1.2}
+      text={subtitle}
+    />
+  </div>
+}
