addb819ae6d3efb3a180be82b7692b2271069b80
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-07-15 00:04:11 -0300

    feat: refactors Bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,60 +1,107 @@
-import { motion } from "framer-motion"
-
-import TitleLeft from "./TitleLeft"
-
 export default function Bio() {
-  return <div className="mt-12 w-full flex flex-col items-center relative">
-    <div className="p-8 w-full max-w-6xl flex flex-col">
-
-      <TitleLeft
-        title="bio"
-        description="My journey as a developer."
-        subtitle="/bio"
-      />
-
-      <motion.div
-        className="text-gray-400 text-sm text-justify hyphens-auto border-r border-gray-700 mt-8 pr-6"
-        initial="hidden"
-        whileInView="visible"
-        variants={list}
-        viewport={{ once: true }}
-      >
-        <motion.p className="mb-2" variants={item} transition={itemTransition}>
-          Hi, I'm Alisson! <span className="text-xl">👋</span>
-        </motion.p>
-        <motion.p className="mb-2" variants={item} transition={itemTransition}>
-          I have degrees in Science and Technology as well as in Production Engineering, both from the Federal Rural University of the Semi-Arid Region in Brazil.
-        </motion.p>
-        <motion.p className="mb-2" variants={item} transition={itemTransition}>
-          Passionate about technology, I've always been driven to find solutions that help companies enter the digital world. That journey eventually led me to dive deep into the world of programming.
-        </motion.p>
-        <motion.p className="mb-2" variants={item} transition={itemTransition}>
-          From working independently to becoming a full-time team member, I've been building my career as a web developer for over five years, working with both backend and frontend technologies.
-        </motion.p>
-      </motion.div>
-
+  return <div className="w-full grid grid-cols-12 grid-row-12 text-[0.6rem] text-gray-500 text-center">
+    <div className="col-span-3 row-span-1 relative flex items-end justify-center p-3 h-[20rem]">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      <div className="text-8xl text-white" style={{ writingMode: 'sideways-lr' }}>
+        B<span className="italic">1</span>O
+      </div>
     </div>
+    <div className="col-span-7 row-span-1 relative flex justify-center items-end">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      timeline
+    </div>
+    <div className="col-span-2 row-span-1 relative flex justify-center items-end">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      5 years
+    </div>
+
+
+    <div className="col-span-3 row-span-1 relative h-[8rem]">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+    </div>
+    <div className="col-span-7 row-span-1 relative text-gray-300">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-700"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      as a developer
+    </div>
+    <div className="col-span-2 row-span-1 relative text-gray-300">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      ago
+    </div>
+
+
+    <div className="col-span-3 row-span-1 relative">
+    </div>
+    <div className="col-span-7 row-span-1 relative py-1 pt-4 pr-4 text-justify text-xs text-gray-400">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      Hi, I'm Alisson! 👋
+    </div>
+
+    <div className="col-span-3 row-span-1 relative text-right text-4xl pr-4 text-gray-700">
+      01
+    </div>
+    <div className="col-span-7 row-span-1 relative py-1 pr-4 text-justify text-xs text-gray-400">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      I have degrees in <b>Science and Technology</b> as well as in <b>Production Engineering</b>, both from the Federal Rural University of the Semi-Arid Region in Brazil.
+    </div>
+
+    <div className="col-span-3 row-span-1 relative text-right text-4xl pr-4 text-gray-700">
+      02
+    </div>
+    <div className="col-span-7 row-span-1 relative py-1 pr-4 text-justify text-xs text-gray-400">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      Passionate about technology, I've always been driven to find solutions that help companies enter the digital world. That journey eventually led me to dive deep into the world of programming.
+    </div>
+
+    <div className="col-span-3 row-span-1 relative text-right text-4xl pr-4 text-gray-700">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      03
+    </div>
+    <div className="col-span-7 row-span-1 relative py-1 pb-4 pr-4 text-justify text-xs text-gray-400">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      From working independently to becoming a full-time team member, I've been building my career as a web developer for <b>over five years</b>, working with both <b>backend</b> and <b>frontend</b> technologies.
+    </div>
+
+    <div className="col-span-2 row-span-4 col-start-11 row-start-3 relative flex justify-end">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      <div style={{ writingMode: 'sideways-lr' }}>/bio</div>
+    </div>
+
+    <div className="col-span-3 row-span-1 relative h-[6rem]">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-700"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      <div className="flex w-full h-full">
+        <div className="align-center" style={{ writingMode: 'sideways-lr' }}>experience</div>
+      </div>
+    </div>
+    <div className="col-span-7 row-span-1 relative flex justify-center items-end">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+      2021 - current
+    </div>
+    <div className="col-span-2 row-span-1 relative">
+      <div className="absolute inset-x-0 bottom-0 h-[1px] bg-gray-700"></div>
+    </div>
+
+
+    <div className="col-span-3 row-span-1 relative text-gray-300 h-[8rem]">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-700"></div>
+      freelancer
+    </div>
+    <div className="col-span-7 row-span-1 relative text-gray-300">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-300"></div>
+      fullstack developer
+    </div>
+    <div className="col-span-2 row-span-1 relative text-gray-300"></div>
+
+
+    <div className="col-span-3 row-span-1 relative h-[12rem]">
+      <div className="absolute inset-y-0 right-0 w-[1px] bg-gray-700"></div>
+    </div>
+    <div className="col-span-7 row-span-1 relative"></div>
+    <div className="col-span-2 row-span-1 relative"></div>
   </div>
-}
-
-const list = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.5,
-    },
-  },
-}
-
-const item = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-}
-
-const itemTransition = {
-  type: "tween",
-  duration: 1,
-  ease: "easeInOut",
 }
