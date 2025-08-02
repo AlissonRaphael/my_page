9ee5b03d0634e3a347f8c8d1bf266da73bab8c9b
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-08-02 19:20:48 -0300

    feat: adds pagination in Repos

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,21 +1,34 @@
 import { useCallback, useEffect, useState } from 'react';
 import Repository from './Repository';
+import { PackagePlus } from 'lucide-react';
+
+const OFFSET = 5
 
 export default function Repos() {
   const [repositories, setRepositories] = useState([])
+  const [showing, setShowing] = useState([])
+  const [page, setPage] = useState(1)
 
   const loadRepositories = useCallback(async () => {
     const response = await fetch('https://api.github.com/users/AlissonRaphael/repos')
     const data = await response.json()
     setRepositories(data)
+    setShowing(data.filter((_, index) => index < OFFSET))
   }, [])
 
   useEffect(() => {
     loadRepositories()
   }, [loadRepositories])
 
-  return <div className="my-24 w-full flex flex-col items-center">
-    <div className="w-full mr-10 flex flex-row justify-end">
+  const handleShowRepositories = useCallback(async page => {
+    const lastIndex = page * OFFSET
+    const showing = repositories.filter((_, index) => index < lastIndex)
+    setShowing(showing)
+    setPage(page)
+  }, [repositories])
+
+  return <div className="mt-60 p-4 w-full flex flex-col items-center">
+    <div className="w-100 flex flex-row justify-end">
       <div className="pr-4 pt-32 border-r-1 border-neutral-500">
         <div className="m-auto" style={{ writingMode: 'sideways-lr' }}>
           <div className="text-left text-7xl font-bold uppercase italic">
@@ -38,26 +51,33 @@ export default function Repos() {
         </div>
       </div>
     </div>
-    <div className="m">
-      {
-        repositories.map(({ id, name, license, description, language, visibility, owner, stargazers_count, watchers_count, forks_count, html_url }) => {
-          const { name: licenseName } = license || {}
-          const { login } = owner || {}
-          return <Repository 
-            key={id}
-            title={name}
-            license={licenseName}
-            description={description}
-            language={language}
-            visibility={visibility}
-            owner={login}
-            stars={stargazers_count}
-            watching={watchers_count}
-            forks={forks_count}
-            url={html_url}
-          />
-        })
-      }
+    <div>
+      { showing.map(({ id, name, license, description, language, visibility, owner, stargazers_count, watchers_count, forks_count, html_url }) => {
+        const { name: licenseName } = license || {}
+        const { login } = owner || {}
+        return <Repository 
+          key={id}
+          title={name}
+          license={licenseName}
+          description={description}
+          language={language}
+          visibility={visibility}
+          owner={login}
+          stars={stargazers_count}
+          watching={watchers_count}
+          forks={forks_count}
+          url={html_url}
+        />
+      })}
     </div>
+      { repositories.length === showing.length ? null : <div className="mt-6 w-full flex">
+        <button 
+          onClick={() => handleShowRepositories(page + 1)}
+          className="cursor-pointer w-100 p-3 rounded-xs text-white flex justify-center items-center gap-2 bg-main opacity-70 hover:opacity-100 transition-all duration-300"
+        >
+          <PackagePlus />
+          See More
+        </button>
+      </div> }
   </div>
 }
