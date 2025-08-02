c60a9af360e898cc1c8176fcf89d8dc4bfea1fc6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-08-02 19:20:25 -0300

    feat: implements Social

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,13 @@
 import { SiGithub, SiMedium, SiGmail } from "@icons-pack/react-simple-icons";
 
 import Social from "./Social";
+import { Linkedin } from "lucide-react";
 
 export default function Footer() {
-  return <div className="my-24 pt-4 w-full flex flex-col items-center bg-black border-t-1 border-neutral-900">
+  return <div className="mt-24 py-6 w-full flex flex-col items-center bg-black border-t-1 border-b-1 border-neutral-900">
     <div className="p-2 w-100 flex justify-end">
-      <div className="text-right text-[0.60rem] text-neutral-300 w-80">
+      <div className="text-right text-[0.70rem] text-neutral-300 w-70">
         You can find me and see more details about my work on several platforms.
-        On LinkedIn, I share professional updates, connections, and experiences.
-        On Medium, I write articles and tutorials about development and learnings
-        along my journey. I also maintain personal projects and experiments on GitHub
       </div>
     </div>
 
@@ -28,20 +26,26 @@ export default function Footer() {
 const SOCIALS = [
   {
     title: "Github",
-    url: "",
+    url: "https://github.com/AlissonRaphael/",
     icon: SiGithub,
     description: "More details about personal projects, repositories, activities and code can be found directly on Github",
   },
   {
+    title: "Linkedin",
+    url: "https://www.linkedin.com/in/alisson-raphael-oliveira/",
+    icon: Linkedin,
+    description: "I share professional updates, connections, and experiences on Linkedin.",
+  },
+  {
     title: "Medium",
-    url: "",
+    url: "https://alissonraphaeloliveira.medium.com/",
     icon: SiMedium,
-    description: "More details about personal projects, repositories, activities and code can be found directly on Github",
+    description: "I write articles and tutorials about development and learnings along my journey on Medium.",
   },
   {
     title: "Email",
-    url: "",
+    url: "mailto:alissonraphaelolivera@gmail.com/",
     icon: SiGmail,
-    description: "More details about personal projects, repositories, activities and code can be found directly on Github",
+    description: "Please feel free to contact us by email if you have any questions or professional interests.",
   },
 ]
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,23 +1,39 @@
+import { motion } from "framer-motion";
+
 export default function Social({ title, url, Icon, description }) {
   return <div className="px-4 w-full h-full">
-    <div className="w-full h-[1px] bg-neutral-600"></div>
+    <div className="w-full h-[1px] bg-neutral-700"></div>
 
     <div className="px-4 py-6 w-full display flex items-center justify-between gap-5">
       <div className="">
         <Icon size={22} color="#fff" />
       </div>
-      <span className="w-4/12 text-white text-3xl">
+      <span className="w-4/12 text-white text-2xl">
         {title}
       </span>
 
-      <div className="w-6/12 flex flex-col gap-2 text-neutral-500">
+      <div className="w-6/12 flex flex-col gap-2 text-neutral-400">
         <p className="text-[0.60rem] text-justify">
           {description}
         </p>
-        <a href={url} className="text-xs">
-          View
-        </a>
+        <Link url={url} />
       </div>
     </div>
   </div>
 }
+
+function Link({ url }) {
+  return <motion.a
+    href={url} className="text-xs text-right tracking-[0.15em]" target="_blank"
+    initial={{ opacity: 0, y: 30 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 2, ease: "easeInOut" }}
+  >
+    <span
+      className="border-b-1 text-neutral-500"
+    >
+      view
+    </span>
+  </motion.a>
+}
+
