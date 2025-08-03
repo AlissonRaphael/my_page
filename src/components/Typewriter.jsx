a4914f7ad6dde7e16d45113e9e794faf568145a9
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-08-03 17:20:27 -0300

    feat: improvements in Typewriter

--- src/components/Typewriter.jsx
+++ src/components/Typewriter.jsx
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
 
-export default function Typewriter({ text, className, initialDelay }) {
+export default function Typewriter({ text, className, initialDelay, once = true }) {
   const textArray = text.split("");
-  let delay = initialDelay || 0.3
+  let delay = initialDelay || 0
 
   return <div className={className}>
     {textArray.map((char, index) => {
-      delay = delay + Math.random() * (0.3 - 0.05) + 0.1
+      delay = delay + Math.random() * (0.1 - 0.05) + 0.1
       return <motion.span
         key={index}
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ delay, ease: 'anticipate', duration: 0.4 }}
-        viewport={{ once: true }}
+        transition={{ delay, ease: 'anticipate', duration: 0.1 }}
+        viewport={{ once }}
       >
         {char}
       </motion.span>
