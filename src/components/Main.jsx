2d5a1c83791239d32fa7f080892b2f81874ca048
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-08-31 17:08:55 -0300

    feat: implements Video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import ExploreAnimation from './ExploreAnimation';
 import Title from './Title';
+import Video from './Video';
 
 export default function Main() {
   return <section className="w-full h-svh z-0 flex flex-col items-center justify-between relative">
     <Header />
     <ExploreAnimation />
     <Title />
+    <Video />
   </section>
 }
---
+++ src/components/Video.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+import Desktop from '../assets/desktop.mp4';
+import Mobile from '../assets/mobile.mp4';
+
+export default function Video() {
+  const [mobile, setMobile] = useState(false)
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (window.innerWidth <= 640) {
+      setMobile(true)
+    }
+  }, [])
+
+  useGSAP(() => {
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#video-container",
+        scroller: "#smooth-wrapper",
+        pin: true,
+        scrub: true,
+        start: "top top",
+        end: "+=1000",
+      }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      timeline.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      }, 0)
+    }
+
+    timeline.fromTo(".shadows", {
+      opacity: 0
+    }, {
+      opacity: 1,
+    }, 0)
+  }, [])
+
+  return <div id="video-container">
+    <video
+      ref={videoRef}
+      src={mobile ? Mobile : Desktop}
+      alt="Alisson Oliveira"
+      muted
+      playsInline
+      preload="auto"
+      className="h-svh aspect-[9/16] sm:aspect-[16/8] object-cover object-center z-0"
+      style={{ willChange: "transform, currentTime" }}
+    />
+    <div
+      id="shadow1"
+      className="shadows absolute z-20 inset-0 w-full h-full"
+      style={{
+        background: "radial-gradient(circle at 50% -20%, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 120%)",
+        opacity: 1,
+      }}
+    ></div>
+    <div
+      id="shadow2"
+      className="shadows absolute z-20 inset-0 w-full h-full"
+      style={{
+        background: "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%)",
+        opacity: 1,
+      }}
+    ></div>
+  </div>
+}
