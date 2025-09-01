b150b5ae411f112df242eff34f64c0dcc6a08e7f
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-08-31 23:01:18 -0300

    feat: review Presentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -16,16 +16,15 @@ export default function Presentation() {
         start: "top 60%",
         end: "bottom bottom",
         scrub: true,
-        markers: true,
       },
     })
   }, [])
 
   return <section id="presentation" className="w-full h-svh flex items-center justify-center relative">
-    <div className="max-w-3xl text-3xl text-justify">
-      <p data-lag="0" className="mb-4">I have degrees in Science and Technology as well as in Production Engineering, both from the Federal Rural University of the Semi-Arid Region in Brazil.</p>
-      <p data-lag="0.2" className="mb-4">Passionate about technology, I've always been driven to find solutions that help companies enter the digital world. That journey eventually led me to dive deep into the world of programming.</p>
-      <p data-lag="0.4">From working independently to becoming a full-time team member, I've been building my career as a web developer for over five years, working with both backend and frontend technologies.</p>
+    <div className="max-w-3xl px-6 md:px-8 lg:px-0 text-lg md:text-xl lg:text-3xl text-justify">
+      <p className="mb-4">I have degrees in Science and Technology as well as in Production Engineering, both from the Federal Rural University of the Semi-Arid Region in Brazil.</p>
+      <p className="mb-4">Passionate about technology, I've always been driven to find solutions that help companies enter the digital world. That journey eventually led me to dive deep into the world of programming.</p>
+      <p>From working independently to becoming a full-time team member, I've been building my career as a web developer for over five years, working with both backend and frontend technologies.</p>
     </div>
   </section>
 }
