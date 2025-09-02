08acac0a349342781e3b68cfaaa0b78899217dd6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-09-01 23:42:30 -0300

    refactor: Title testing

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { DynamicIcon } from "lucide-react/dynamic";
 
 export default function Title() {
   useGSAP(() => {
@@ -12,41 +13,72 @@ export default function Title() {
     })
 
     gsap.to("#a-scramble", {
-      delay: 2,
-      duration: 2,
-      repeat: -1,
-      repeatDelay: 4,
-      scrambleText: {
-        text: "a",
-        chars: "@a7",
-      }
-    })
-
-    gsap.to("#v-scramble", {
       delay: 1.5,
       duration: 1.2,
       repeat: -1,
       repeatDelay: 6,
       scrambleText: {
-        text: "v",
-        chars: "u",
+        text: "a",
+        chars: "71",
       }
+    })
+
+    gsap.to("#o-scramble", {
+      delay: 2,
+      duration: 2,
+      repeat: -1,
+      repeatDelay: 4,
+      scrambleText: {
+        text: "o",
+        chars: "+",
+      }
+    })
+
+    gsap.to("#e-scramble", {
+      delay: 1,
+      duration: 1.3,
+      repeat: -1,
+      repeatDelay: 2,
+      scrambleText: {
+        text: "e",
+        chars: "b4",
+      }
+    })
+
+    gsap.from("#icon", {
+      rotate: "259deg",
+      ease: "linear",
+      scrollTrigger: {
+        trigger: "#title",
+        scroller: "#smooth-wrapper",
+        start: "top top",
+        end: "bottom center",
+        scrub: true,
+      },
     })
   }, [])
 
   return <div id="title" className="absolute z-10 inset-0 flex items-center justify-center">
-    <div>
-      <span className="font-[Birthstone] font-bold text-5xl sm:text-5xl md:text-7xl lg:text-[6rem]">Hi! I'm </span><br />
-      <div className="pl-2 sm:pl-4 md:pl-6 lg:pl-8 font-[Subjectivity] font-extrabold uppercase tracking-tighter text-7xl leading-16 sm:text-[7rem] sm:leading-24 md:text-[9rem] md:leading-30 lg:text-[12rem] lg:leading-40">
-        <span className="relative">
-          alisson
-          <span id="a-scramble" className="absolute w-fit font-[Birthstone] -top-1 lg:-top-2 -left-2 sm:-left-4 md:-left-7 lg:-left-10 text-[8rem] sm:text-[12rem] md:text-[16rem] lg:text-[24rem] font-extralight text-main">A</span>
-        </span>
-        <br />
-        <span className="pl-12 sm:pl-20 md:pl-26 lg:pl-36 relative">
-          oliveira
-          <span id="v-scramble" className="absolute w-fit font-[Birthstone] -top-1 lg:-top-2 right-42 sm:right-66 md:right-82 lg:right-108 text-[8rem] sm:text-[12rem] md:text-[16rem] lg:text-[24rem] font-extralight text-main">V</span>
-        </span>
+    <div className="font-[Poppins] font-extrabold uppercase border-2 border-orange-500 text-4xl lg:p-14">
+      <div className="grid grid-cols-4 grid-rows-2 justify-self-center text-center align-middle p-6 gap-y-6 gap-x-10 lg:gap-y-14 lg:gap-x-22">
+        <div id="a-scramble">a</div>
+        <div>l</div>
+        <div>i</div>
+        <div>s</div>
+        <div>s</div>
+        <div id="o-scramble">o</div>
+        <div>n</div>
+        <div id="icon" className="flex items-center justify-center">
+          <DynamicIcon name="aperture" className="stroke-orange-500" size={28} />
+        </div>
+        <div>o</div>
+        <div>l</div>
+        <div>i</div>
+        <div>v</div>
+        <div id="e-scramble">e</div>
+        <div>i</div>
+        <div>r</div>
+        <div>a</div>
       </div>
     </div>
   </div>
