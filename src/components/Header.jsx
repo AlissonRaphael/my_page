a6644ab5aa0b801b14e51c6c1454a08b0dd3021a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-09-06 16:52:27 -0300

    feat: adjusts Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,29 +17,44 @@ export default function Header() {
 
   }, [])
 
-  return <header id="header" className="absolute inset-x-0 top-0 z-30 p-2 w-full flex flex-col justify-between items-center text-white backdrop-blur-md">
-    <div className="font-extrabold uppercase text-[10vw] tracking-[1rem] leading-[8rem] pb-4">
-      <span className="text-main">
-        alisson
-      </span>
-      <span>
-        oliveira
-      </span>
+  return <header id="header" className="absolute inset-x-0 top-0 z-30 p-2 w-full flex flex-col justify-between items-center text-white backdrop-blur-lg">
+    <div className="font-extrabold uppercase text-[8rem] tracking-[1rem] leading-[6rem] w-full flex">
+      <div className="flex items-center justify-between w-1/2 text-main">
+        <span>a</span>
+        <span>l</span>
+        <span>i</span>
+        <span>s</span>
+        <span>s</span>
+        <span>o</span>
+        <span>n</span>
+        <span>*</span>
+      </div>
+      <div className="flex items-center justify-between w-1/2">
+        <span>o</span>
+        <span>l</span>
+        <span>i</span>
+        <span>v</span>
+        <span>e</span>
+        <span>i</span>
+        <span>r</span>
+        <span>a</span>
+        <span>.</span>
+      </div>
     </div>
     <ul className="mt-4 w-full flex gap-4 items-center justify-between text-xs tracking-[1px]">
       <li className="pt-1 w-full border-t-1 border-white">
         <span className="opacity-70 ">Published in 2025.</span>
       </li>
       <li className="pt-1 w-full border-t-1 border-white uppercase">
-        <span className="h-full w-[2px] bg-white rounded-[1px] mr-1">&ensp;</span>
+        <span className="h-full w-[2px] bg-white rounded-[2px] mr-1">&ensp;</span>
         <a href="/#start">start</a>
       </li>
       <li className="pt-1 w-full border-t-1 border-white uppercase">
-        <span className="h-full w-[2px] bg-white rounded-[1px] mr-1">&ensp;</span>
+        <span className="h-full w-[2px] bg-white rounded-[2px] mr-1">&ensp;</span>
         <a href="/#about">about</a>
       </li>
       <li className="pt-1 w-full border-t-1 border-white uppercase">
-        <span className="h-full w-[2px] bg-white rounded-[1px] mr-1">&ensp;</span>
+        <span className="h-full w-[2px] bg-white rounded-[2px] mr-1">&ensp;</span>
         <a href="/#repo">repos</a>
       </li>
     </ul>
