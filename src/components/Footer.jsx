1b6f14b9e0500c2ff3eb3c7c0724cf6e8b8125aa
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-09-08 22:16:18 -0300

    feat: adjusts Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,76 @@
-import { motion } from "framer-motion";
 import { SiGithub, SiMedium, SiGmail } from "@icons-pack/react-simple-icons";
-import { Linkedin } from "lucide-react";
+import { ArrowUp, ArrowUpRight, Linkedin, SquareArrowUp } from "lucide-react";
 
 import Social from "./Social";
 
 export default function Footer() {
-  return <div className="mt-24 py-6 w-full flex flex-col items-center bg-black border-t-1 border-b-1 border-neutral-900">
-    <div className="p-2 w-100 flex justify-end">
-      <div className="text-right text-[0.70rem] text-neutral-300 w-70">
-        You can find me and see more details about my work on several platforms.
+  return <footer id="footer" className="p-4 lg:p-14 w-full flex items-center justify-around gap-14 bg-black border-t-1 border-neutral-900">
+    <div className="w-7/12 max-w-3xl">
+      <div className="font-[FreightBig] text-4xl text-white uppercase">
+        <span className="lowercase italic">you can </span>
+        find me
+        <span className="lowercase italic"> and see more </span>
+        details about
+        <span className="lowercase italic"> my work on </span>
+        several platforms.
+      </div>
+      <p className="mt-2 text-neutral-400 text-base text-justify hyphens-auto">
+        More details about personal projects, repositories, activities and code can be found directly on Github.
+        Connect with me on LinkedIn, where I share professional updates and experiences.
+        Articles and tutorials about development and learnings along my journey on Medium.
+        Please feel free to contact us by email if you have any questions or professional interests.
+      </p>
+
+      <div className="mt-4">
+        <a href="#home" className="cursor-pointer w-fit py-2 px-4 rounded-xs text-white gap-1 flex justify-center items-center bg-main opacity-60 hover:opacity-100 transition-all duration-300">
+          <ArrowUpRight />
+          Go back to top
+        </a>
+      </div>
+
+      <div className="mt-12 text-neutral-500 text-base">
+        © 2025 Your Name. All rights reserved. No part of this site may be reproduced without permission.
       </div>
     </div>
 
-    <div className="mt-10">
-      {SOCIALS.map(({ title, url, icon, description }) => <Social
-        key={title}
+    <div className="w-5/12">
+      {SOCIALS.map(({ id, title, url, icon, description }) => <Social
+        id={id}
+        key={id}
         title={title}
         description={description}
         url={url}
         Icon={icon}
       />)}
     </div>
-  </div>
+  </footer>
 }
 
 const SOCIALS = [
   {
-    title: "Github",
+    id: "github",
+    title: "Projects",
     url: "https://github.com/AlissonRaphael/",
     icon: SiGithub,
-    description: "More details about personal projects, repositories, activities and code can be found directly on Github",
+    description: "More details about personal projects, repositories, activities and code can be found directly on Github.",
   },
   {
-    title: "Linkedin",
+    id: "linkedin",
+    title: "Network",
     url: "https://www.linkedin.com/in/alisson-raphael-oliveira/",
     icon: Linkedin,
     description: "Connect with me on LinkedIn, where I share professional updates and experiences.",
   },
   {
-    title: "Medium",
+    id: "medium",
+    title: "Articles",
     url: "https://alissonraphaeloliveira.medium.com/",
     icon: SiMedium,
     description: "Articles and tutorials about development and learnings along my journey on Medium.",
   },
   {
-    title: "Email",
+    id: "email",
+    title: "Contact",
     url: "mailto:alissonraphaelolivera@gmail.com/",
     icon: SiGmail,
     description: "Please feel free to contact us by email if you have any questions or professional interests.",
