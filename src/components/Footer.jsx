07f9bd6fe6d76064de567cae27f56df1caed5435
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-09-12 23:59:53 -0300

    feat: add TextFade in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,29 @@ import { SiGithub, SiMedium, SiGmail } from "@icons-pack/react-simple-icons";
 import { ArrowUpRight, Linkedin } from "lucide-react";
 
 import Social from "./Social";
+import TextFade from "./animations/TextFade";
 
 export default function Footer() {
   return <footer id="footer" className="p-4 md:p-8 lg:p-14 w-full flex flex-col lg:flex-row items-center justify-around gap-8 lg:gap-18 xl:gap-24 2xl:gap-32 bg-black border-t-1 border-neutral-900">
     <div className="w-full lg:w-1/2 lg:max-w-3xl">
       <div className="font-[FreightBig] text-xl md:text-4xl text-white uppercase">
-        <span className="lowercase italic">you can </span>
-        find me
-        <span className="lowercase italic"> and see more </span>
-        details about
-        <span className="lowercase italic"> my work on </span>
-        several platforms.
+        <TextFade>
+          <span className="lowercase italic">you can </span>
+          find me
+          <span className="lowercase italic"> and see more </span>
+          details about
+          <span className="lowercase italic"> my work on </span>
+          several platforms.
+        </TextFade>
       </div>
-      <p className="mt-1 sm:mt-2 text-neutral-400 text-xs sm:text-sm lg:text-base text-justify hyphens-auto">
-        More details about personal projects, repositories, activities and code can be found directly on Github.
-        Connect with me on LinkedIn, where I share professional updates and experiences.
-        Articles and tutorials about development and learnings along my journey on Medium.
-        Please feel free to contact us by email if you have any questions or professional interests.
-      </p>
+      <TextFade>
+        <p className="mt-1 sm:mt-2 text-neutral-400 text-xs sm:text-sm lg:text-base text-justify hyphens-auto">
+          More details about personal projects, repositories, activities and code can be found directly on Github.
+          Connect with me on LinkedIn, where I share professional updates and experiences.
+          Articles and tutorials about development and learnings along my journey on Medium.
+          Please feel free to contact us by email if you have any questions or professional interests.
+        </p>
+      </TextFade>
 
       <div className="mt-4">
         <a href="#home" className="cursor-pointer w-fit py-2 px-4 rounded-xs text-white gap-1 flex justify-center items-center bg-main opacity-60 hover:opacity-100 transition-all duration-300">
