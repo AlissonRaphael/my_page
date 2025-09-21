8e02a5af57aea687fb33a0cdac3d12f4bfd2d6d3
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-09-21 19:48:52 -0300

    fix: improvemnts in mobile performance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { ArrowUpRight, Linkedin } from "lucide-react";
 
 import Social from "./Social";
 import TextFade from "./animations/TextFade";
+import ContentFade from "./animations/ContentFade";
 
 export default function Footer() {
   return <footer id="footer" className="p-4 md:p-8 lg:p-14 w-full flex flex-col lg:flex-row items-center justify-around gap-8 lg:gap-18 xl:gap-24 2xl:gap-32 bg-black border-t-1 border-neutral-900">
@@ -17,14 +18,14 @@ export default function Footer() {
           several platforms.
         </TextFade>
       </div>
-      <TextFade>
+      <ContentFade>
         <p className="mt-1 sm:mt-2 text-neutral-400 text-xs sm:text-sm lg:text-base text-justify hyphens-auto">
           More details about personal projects, repositories, activities and code can be found directly on Github.
           Connect with me on LinkedIn, where I share professional updates and experiences.
           Articles and tutorials about development and learnings along my journey on Medium.
           Please feel free to contact us by email if you have any questions or professional interests.
         </p>
-      </TextFade>
+      </ContentFade>
 
       <div className="mt-4">
         <a href="#home" className="cursor-pointer w-fit py-2 px-4 rounded-xs text-white gap-1 flex justify-center items-center bg-main opacity-60 hover:opacity-100 transition-all duration-300">
